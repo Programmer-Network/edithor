8e9e0e49439bfb6537c628ee6b3cb8d60475b4a1
Nora Söderlund <norasoderlund@icloud.com>
2023-01-12 21:06:15 +0100

    Super duper important log lines

--- src/Controllers/Rules/enable-header-elements.ts
+++ src/Controllers/Rules/enable-header-elements.ts
@@ -17,15 +17,12 @@ export default class EnableHeaderElements implements EdithorRule {
     conditions: EdithorRuleStates;
 
     process(input: string): string {
-        console.log("enable hjeader elements");
         const character = `&#${(this.options?.character ?? '#').charCodeAt(0)};`;
         const minimumDepth = this.options?.minimumDepth ?? 1;
         const maximumDepth = this.options?.maximumDepth ?? 4;
 
         for(let depth = maximumDepth; depth != minimumDepth - 1; depth--) {
             const regExp = new RegExp(`${Array(depth).fill(character).join('')} (.*)`, 'g');
-
-            console.log(regExp);
 
             input = input.replaceAll(regExp, `<h${depth}>$1</h${depth}>`);
         }
