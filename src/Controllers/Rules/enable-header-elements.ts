9034bc86040b42aea81d61372ff092c4456472ba
Nora Söderlund <norasoderlund@icloud.com>
2023-01-12 22:27:23 +0100

    Low scale header elements performance improvements

--- src/Controllers/Rules/enable-header-elements.ts
+++ src/Controllers/Rules/enable-header-elements.ts
@@ -19,11 +19,15 @@ export default class EnableHeaderElements implements EdithorRule {
     process(input: string): string {
         const character = `&#${(this.options?.character ?? '#').charCodeAt(0)};`;
         const minimumDepth = this.options?.minimumDepth ?? 1;
-        const maximumDepth = this.options?.maximumDepth ?? 4;
+        const maximumDepth = this.options?.maximumDepth ?? 6;
 
         for(let depth = maximumDepth; depth != minimumDepth - 1; depth--) {
-            const regExp = new RegExp(`${Array(depth).fill(character).join('')} (.*)`, 'g');
+            const syntax = Array(depth).fill(character).join('');
 
+            if(!input.includes(syntax))
+                continue;
+
+            const regExp = new RegExp(`${syntax} (.*)`, 'g');
             input = input.replaceAll(regExp, `<h${depth}>$1</h${depth}>`);
         }
 
